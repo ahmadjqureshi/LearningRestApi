8d8feb11642b921e313b5457719d33ca02675de8
ahmadjqureshi <ahmad.j.qureshi@gmail.com>
2021-05-27 14:56:26 +0200

    Details page added.

---
+++ EmployeeWebApp/Pages/EmployeeDetailsBase.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeModels;
+using EmployeeWebApp.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApp.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        public Employee Emp { get; set; } = new Employee();
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            int id = int.Parse(Id);
+
+            Emp = await EmployeeService.GetEmployee(id);
+        }
+    }
+}
--- EmployeeWebApp/Services/IEmployeeService.cs
+++ EmployeeWebApp/Services/IEmployeeService.cs
@@ -9,5 +9,6 @@ namespace EmployeeWebApp.Services
     public interface IEmployeeService
     {
         Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int id);
     }
 }
